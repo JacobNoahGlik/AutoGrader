9caaa64f6b18bbadc6d31170f87717a6cb1a5613
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-06-10 22:18:13 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from auto_grader_ai import Auto_Grader_AI
 
-AI = Auto_Grader_AI('Creativity: High, Medium, Low')
+rubric: str = 'Creativity: High, Medium, Low'
+submission: str = 'My name is Jacob, I am a sophamore at WPI. I am a computer science major. I am interested in machine learning and artificial inteligence. I am currently working on a project that uses machine learning to create artificial inteligence. I think I would be great for this pogram!'
 
-grade = AI.grade('My name is Jacob, I am a sophamore at WPI. I am a computer science major. I am interested in machine learning and artificial inteligence. I am currently working on a project that uses machine learning to create artificial inteligence. I think I would be great for this pogram!')
+AI = Auto_Grader_AI(rubric)
+
+grade = AI.grade(submission)
 
 print(grade)
