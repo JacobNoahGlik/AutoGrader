950a1290e2e109d8461055d3cb2751db254f8e4f
Jacob Glik <70964953+JacobNoahGlik@users.noreply.github.com>
2024-06-10 22:08:42 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+from auto_grader_ai import Auto_Grader_AI
+
+AI = Auto_Grader_AI('Creativity: High, Medium, Low')
+
+grade = AI.grade('My name is Jacob, I am a sophamore at WPI. I am a computer science major. I am interested in machine learning and artificial inteligence. I am currently working on a project that uses machine learning to create artificial inteligence. I think I would be great for this pogram!')
+
+print(grade)
